f64f2116598f17773811d3109bd753525c072b91
Sergio A. Adamoli <sergio@aiperkiu.com>
2021-04-27 15:57:40 -0300

    Login traducido

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,11 +9,11 @@
                 <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
                     <div class="w-full">
                         <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-                            Login
+                            Ingresar
                         </h1>
                         @if ($errors->any())
                         <div class="mb-4">
-                            <div class="font-medium text-red-600">Whoops! Something went wrong.</div>
+                            <div class="font-medium text-red-600">Whoops! Algo extraño ocurrio.</div>
 
                             <ul class="mt-3 text-sm text-red-600 list-disc list-inside">
                                 @foreach ($errors->all() as $error)
@@ -35,21 +35,21 @@
                                 <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="Jane Doe" name="email" value="{{ old('email') }}" required autofocus />
                             </label>
                             <label class="block mt-4 text-sm">
-                                <span class="text-gray-700 dark:text-gray-400">Password</span>
+                                <span class="text-gray-700 dark:text-gray-400">Contraseña</span>
                                 <input class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input" placeholder="***************" type="password" name="password" required autocomplete="current-password" />
                             </label>
                             <label class="block mt-4 text-sm">
-                                <input type="checkbox" class="form-checkbox" name="remember"> <span class="ml-1 text-gray-700 dark:text-gray-400">{{ __('Remember me') }}</span>
+                                <input type="checkbox" class="form-checkbox" name="remember"> <span class="ml-1 text-gray-700 dark:text-gray-400">{{ __('Recordarme') }}</span>
                             </label>
                             <!-- You should use a button here, as the anchor is only used for the example  -->
                             <button class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple" type="submit">
-                                {{ __('Login') }}
+                                {{ __('Ingresar') }}
                             </button>
                         </form>
 
                         <p class="mt-4">
                             <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="{{ route('password.request') }}">
-                                {{ __('Forgot your password?') }}
+                                {{ __('¿Olvidaste la contraseña?') }}
                             </a>
                         </p>
                     </div>
